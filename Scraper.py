891751f38fcf9e16487de0c536b742297cfc7c0b
d-alfa <dargis.alfonsas@gmail.com>
2023-10-09 20:49:09 +0300

    One page Scrape

--- Scraper.py
+++ Scraper.py
@@ -2,69 +2,59 @@
 import requests
 import mysql.connector
 from bs4 import BeautifulSoup
-import json
+from time import sleep
+from random import randint
 
 # Request URl
-# def scrape_items(url):
-# 	response = requests.get(url)
-# 	soup = BeautifulSoup(response.text, "html.parser")
-# 	items = soup.find_all("arbata")
-	# all_items = []
-	# for item in items:
-		# item_data = (get_title(item),get_price(item),get_type(item))
-		# all_items.append(item_data)
-	# save_items(all_items)
+def scrape_items(url):
+	response = requests.get(url)
+	soup = BeautifulSoup(response.text, "html.parser")
+	items = soup.find_all(class_="products__item-link")
+	all_items = []
+	for item in items:
+		item_data = (pavadinimo_gavimas(item),kainos_gavimas(item),tipo_gavimas(item))
+		all_items.append(item_data)
+		# sleep(randint(7,67))
+	save_items(all_items)
+	print(all_items)
 
-j_a = open("juodoji-arbata.html", encoding="utf-8")
-soup = BeautifulSoup(j_a, "html.parser")
+# Duomenų išsaugojimas
+def save_items(all_items):
+	connection = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="$improver44",
+        database="arbatos"
+    )
+	c = connection.cursor()
+	# Sukuria table "Arbatos" tarp nurodytos duomenų bazės
+	c.execute('''CREATE TABLE Arbatos 
+		(Pavadinimas VARCHAR(255),Kaina FLOAT(4,2) NOT NULL,Tipas VARCHAR(255))''')
+	c.executemany("INSERT INTO Arbatos VALUES (%s,%s,%s)", all_items)
+	connection.commit()
+	connection.close()
 
-# # title
-# title = soup.find_all("h2")
-# parent = title[0].parent
-# h2 = parent.find("h2")
-# print(h2.string.strip())
+# pavadinimas
+def pavadinimo_gavimas(item):
+	pavadinimas = item.find("h2")
+	return pavadinimas.string.strip()
 
-# # kaina
-# kaina = soup.find_all("ins")
-# parent = kaina[0].parent
-# ins = parent.find("ins")
-# print(ins.string.strip())
+# kaina
+def kainos_gavimas(item):
+	kaina = item.find("ins")
+	kaina2 = kaina.string.replace("\xa0€","").replace(",",".")
+	kaina_float = float(kaina2)
+	return kaina_float
 
 # tipas
-tipas = json.loads(soup.find("script", type="text/javascript").text)
-print(tipas["articleBody"])
+def tipo_gavimas(item):
+	pavadinimai = []
+	tipas = item.find("h2")
+	tipas2 = tipas.string.strip()
+	pavadinimai.append(tipas2)
+	for p in pavadinimai:
+		if "Juodoji" in p or "juodoji" in p:
+			return "Juodoji Arbata"
+	return None
 
-
-
-# def save_items(all_items):
-# 	connection = mysql.connector.connect(
-#         host="localhost",
-#         user="root",
-#         password="$improver44",
-#         database="arbatos"
-#     )
-# 	c = connection.cursor()
-# 	c.execute('''CREATE TABLE Arbatos 
-# 		(Pavadinimas TEXT,Kaina REAL,Tipas Text)''')
-# 	c.executemany("INSERT INTO Arbatos VALUES (%s,%s,%s)", all_items)
-# 	connection.commit()
-# 	connection.close()
-	
-# def get_title(item):
-# 	return item.find("h2").find("products_item-title")["title"]
-
-# <h2 class="products__item-title">
-#                 Juodoji arbata SKONIS IR KVAPAS DARJEELING FTGFOP1
-#             </h2>
-
-# def get_price(item):
-# 	price = item.select(".price_color")[0].get_text()
-# 	return price
-
-# def get_type(item):
-# 	ratings = {"Zero": 0, "One": 1, "Two": 2, "Three": 3, "Four": 4, "Five": 5}
-# 	paragraph = book.select(".star-rating")[0]
-# 	word = paragraph.get_attribute_list("class")[-1]
-# 	return ratings[word]
-
-# scrape_items("https://www.skonis-kvapas.lt/arbata/juodoji-arbata")
+scrape_items("https://www.skonis-kvapas.lt/arbata/juodoji-arbata")
