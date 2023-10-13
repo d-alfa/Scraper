b6edc4684834549f69811cd99e6ef40ccc384f6d
d-alfa <dargis.alfonsas@gmail.com>
2023-10-13 20:14:42 +0300

    Placed list from outside of the function inside

--- Pages.py
+++ Pages.py
@@ -1,8 +1,8 @@
 
 # Returns "juodoji-arbata" pages and puts them inside a list
-j_a_pages = []
 def juodoji_arbata_pages():
     pages_sk = 1
+    j_a_pages = []
     while pages_sk <= 3:
         url = f"https://www.skonis-kvapas.lt/arbata/juodoji-arbata?page={pages_sk}"
         pages_sk = pages_sk + 1
