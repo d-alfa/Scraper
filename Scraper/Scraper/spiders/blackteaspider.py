5915e478dbcb6ca48b35882ede860bafcf749091
Alfonsas Dargis <dargis.alfonsas@gmail.com>
2023-11-10 17:44:31 +0200

    Update blackteaspider

--- Scraper/Scraper/spiders/blackteaspider.py
+++ Scraper/Scraper/spiders/blackteaspider.py
@@ -6,4 +6,11 @@ class BlackteaspiderSpider(scrapy.Spider):
     start_urls = ["https://www.skonis-kvapas.lt/arbata/juodoji-arbata"]
 
     def parse(self, response):
-        pass
+        blackteas = response.css("div.products__item")
+
+        for tea in blackteas:
+            yield{
+                "name" : tea.css("h2::text").get().strip(),
+                "price" : tea.css("ins::text").replace("\xa0€","").replace(",",".").replace("€",""),
+                "url" : tea.css("a.products__item-link").attrib["href"]
+            }
