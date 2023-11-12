d348dc8baa47872b37b7d360c71f646f7c85933f
Alfonsas Dargis <dargis.alfonsas@gmail.com>
2023-11-12 18:34:09 +0200

    Enable pipelines

--- Scraper/Scraper/settings.py
+++ Scraper/Scraper/settings.py
@@ -62,9 +62,9 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    "Scraper.pipelines.ScraperPipeline": 300,
-#}
+ITEM_PIPELINES = {
+   "Scraper.pipelines.Black_Tea_Scraper_Pipeline": 300,
+}
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
